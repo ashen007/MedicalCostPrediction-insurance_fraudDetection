1fbcd6324e93b77c95be21d51eac8f6e37663d75
ashen007 <asheniranga@gmail.com>
2021-02-11 20:31:00 +0530

    implement regression

--- regression_model.py
+++ regression_model.py
@@ -2,8 +2,8 @@ import numpy as np
 
 
 def regression(x, y):
-    c, i = np.linalg.lstsq(x, y, rcond=None)[0]
-    return c, i
+    w = np.linalg.lstsq(x, y, rcond=None)[0]
+    return w
 
 
 def cost_function(x, y, theta):
