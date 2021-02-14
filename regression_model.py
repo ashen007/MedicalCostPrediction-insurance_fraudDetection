1d8da347cd613aeb7b213ee70db0a333e701d599
ashen007 <asheniranga@gmail.com>
2021-02-14 16:31:09 +0530

    optimization - L2 panelty ridge regression function add bias to avoid overfit and gain more accuracy

--- regression_model.py
+++ regression_model.py
@@ -10,6 +10,10 @@ def cost_function(x, y, theta):
     return np.sum((np.dot(x, theta) - y) ** 2) / 2 * len(y)
 
 
+def ridge_cost_function(x, y, theta, alpha):
+    return np.sum(((np.dot(x, theta) - y) ** 2 + (alpha * theta))) / len(y)
+
+
 def gradient_decent(x, y, learning_rate, theta, epochs):
     cost_history = []
 
@@ -24,8 +28,22 @@ def gradient_decent(x, y, learning_rate, theta, epochs):
     return theta, cost_history
 
 
+def ridge_regression(x, y, learning_rate, theta, alpha, epochs):
+    cost_history = []
+
+    for i in range(epochs):
+        hypothesis = np.dot(x, theta)
+        loss = hypothesis - y
+        gradient = 2 * (np.dot(x.T, loss) + alpha * hypothesis)
+        theta = theta - learning_rate * gradient
+        cost = ridge_cost_function(x, y, theta, alpha)
+        cost_history.append(cost)
+
+    return theta, cost_history
+
+
 def fit(x, y, learning_rate, epochs):
     theta = np.zeros(x.shape[1])
-    theta,cost = gradient_decent(x,y,learning_rate,theta,epochs)
+    theta, cost = gradient_decent(x, y, learning_rate, theta, epochs)
 
-    return theta,cost
+    return theta, cost
