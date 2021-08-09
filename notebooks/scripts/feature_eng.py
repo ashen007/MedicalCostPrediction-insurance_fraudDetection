98726077347e6f42374c224d3752ea1f3e0f6bcc
ashen007 <asheniranga@gmail.com>
2021-08-09 21:31:11 +0530

    load data into feature engineering

--- notebooks/scripts/feature_eng.py
+++ notebooks/scripts/feature_eng.py
@@ -1,5 +1,3 @@
-import numpy as np
-import pandas as pd
 from feature_engine.imputation import CategoricalImputer, MeanMedianImputer, ArbitraryNumberImputer
 from feature_engine.encoding import CountFrequencyEncoder, OrdinalEncoder, RareLabelEncoder
 
