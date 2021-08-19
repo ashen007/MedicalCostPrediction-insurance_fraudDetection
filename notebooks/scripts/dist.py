4b64d889c89d8f8240ddca8d6c2416fd20668560
ashen007 <asheniranga@gmail.com>
2021-08-19 10:05:10 +0530

    initialize distribution analysis

---
+++ notebooks/scripts/dist.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+from scipy import stats as ss
+from matplotlib import pyplot as plt
+
+
+class InitDist:
+    """
+    hold data for distribution analysis
+    """
+
+    def __init__(self, dataframe):
+        super().__init__()
+        self.data = dataframe
+
+    def render(self, size, dpi, subplots=False, sub_count=(1, 1)):
+        """
+        create graphs
+        :return:
+        """
+        if not subplots:
+            return plt.Figure(figsize=size, dpi=dpi)
+        else:
+            fig, axes = plt.subplots(nrows=sub_count[0],
+                                     ncols=sub_count[1],
+                                     figsize=size,
+                                     dpi=dpi)
+            return fig, axes
+
+    def __getattribute__(self, item):
+        """
+        get properties
+        :param item:
+        :return:
+        """
+        if item == 'data':
+            return super().__getattribute__('data')
+
+        return super().__getattribute__(item)
